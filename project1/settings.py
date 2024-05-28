a0b55d05e78aeaa6e14296049b86898525b0c2b2
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-05-28 21:01:03 +0300

    login

--- project1/settings.py
+++ project1/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
     
 
 ]
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'project1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
