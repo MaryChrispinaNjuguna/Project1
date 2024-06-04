e33291e9749415f303481f7c13189f2b4a61e742
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-06-04 08:50:16 +0300

    fixed url error

--- project1/urls.py
+++ project1/urls.py
@@ -20,4 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include ("django.contrib.auth.urls")),
-    path('registration/login/', include('login.urls')),]
+]
